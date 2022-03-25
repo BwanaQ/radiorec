f6701a1e5a89b5b9438a6e2e447ac36c625542a8
BwanaQ <thunjawax@gmail.com>
2022-03-25 15:31:36 +0300

    add command at entry point of segmenter with 300s interval

--- inaSpeechSegmenter/script.sh
+++ inaSpeechSegmenter/script.sh
@@ -2,7 +2,6 @@
 set -x
 while true
 do
-    # ina_speech_segmenter.py -i "/root/Recordings/*.mp3" -o "/root/Recordings/" -g "false" -d "smn"
-    echo "it works"
-    sleep 30
+    ina_speech_segmenter.py -i "/root/Recordings/*.mp3" -o "/root/Recordings/" -g "false" -d "smn"
+    sleep 300
 done
